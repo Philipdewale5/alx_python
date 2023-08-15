c35f3cee64ba3f64d3bb3d850c382342c58b32b9
Philipdewale5 <111074724+Philipdewale5@users.noreply.github.com>
2023-08-15 23:45:10 +0100

    Update 6-print_comb3.py

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,6 +1,6 @@
 for x in range(10):
     for y in range(x + 1, 10):
-        print(f"{:02}, {:02}".format(x ,y), end="")
+        print("{:02}, {:02}".format(x ,y), end="")
         if x != 8 or y != 9:
             print(", ", end="")
         else:
