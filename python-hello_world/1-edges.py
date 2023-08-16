477b6a13a97e7c43186a2df8c46da07d7b597cdf
Philipdewale5 <111074724+Philipdewale5@users.noreply.github.com>
2023-08-16 12:23:57 +0100

    Update 1-edges.py

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-word = "Holberton"
-word_first_3 = 	"Hol"
-word_last_2 = "on"
-middle_word = "olberto"
+word = "Adequately"
+word_first_3 = 	"Ade"
+word_last_2 = "ly"
+middle_word = "dequatel"
 print("First 3 letters: {}".format(word_first_3))
 print("Last 2 letters: {}".format(word_last_2))
 print("Middle word: {}".format(middle_word))
