f69ed725f21dc7cd4bf0b00d0f9a811668ed1e5d
Philipdewale5 <111074724+Philipdewale5@users.noreply.github.com>
2023-08-16 11:05:34 +0100

    Update 6-print_comb3.py

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,7 +1,6 @@
 for x in range(10):
-    for y in range(x + 1, 10):
-        print("{}, {}".format(x ,y), end="")
-        if x != 8 or y != 9:
-            print(", ", end="")
-        else:
-            print()
+    for y in range(10):
+        if x = y:
+            print () 
+        elif:
+            print("{}{}".format(x,y), end="")
